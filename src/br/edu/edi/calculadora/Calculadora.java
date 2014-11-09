67705e552777d1f5c9a754e7379442737f200469
Matheus Mahl <matheusmahl@outlook.com>
2014-11-09 14:03:40 -0200

    operadores especiais 09/11/2014

--- src/br/edu/edi/calculadora/Calculadora.java
+++ src/br/edu/edi/calculadora/Calculadora.java
@@ -35,6 +35,7 @@ public class Calculadora extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtAreaCalculo;
 	StringBuffer str = new StringBuffer();
+	private JButton btnIgual;
 
 	/**
 	 * Create the frame.
@@ -43,16 +44,21 @@ public class Calculadora extends JFrame {
 		setResizable(false);
 		setTitle("Calculadora");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 363, 547);
+		setBounds(100, 100, 400, 547);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
 		// Ativa a utilização de teclas fisicas
 		entradaTeclado();
+		
+		
 		/*
 		 * Numerais
 		 */
+		
+		
 		// Botão 0
 		JButton btnZero = new JButton("0");
 		btnZero.setFont(new Font("Arial", Font.BOLD, 24));
@@ -62,8 +68,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnZero.setBounds(111, 455, 52, 51);
+		btnZero.setBounds(146, 455, 52, 51);
 		contentPane.add(btnZero);
+		
 		// Botão 1
 		JButton btnUm = new JButton("1");
 		btnUm.setFont(new Font("Arial", Font.BOLD, 24));
@@ -73,8 +80,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnUm.setBounds(111, 393, 52, 51);
+		btnUm.setBounds(146, 393, 52, 51);
 		contentPane.add(btnUm);
+		
 		// Botão 2
 		JButton btnDois = new JButton("2");
 		btnDois.setFont(new Font("Arial", Font.BOLD, 24));
@@ -84,8 +92,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnDois.setBounds(173, 393, 52, 51);
+		btnDois.setBounds(208, 393, 52, 51);
 		contentPane.add(btnDois);
+		
 		// Botão 3
 		JButton btnTres = new JButton("3");
 		btnTres.setFont(new Font("Arial", Font.BOLD, 24));
@@ -95,8 +104,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnTres.setBounds(233, 393, 52, 51);
+		btnTres.setBounds(270, 393, 52, 51);
 		contentPane.add(btnTres);
+		
 		// Botão 4
 		JButton btnQuatro = new JButton("4");
 		btnQuatro.setFont(new Font("Arial", Font.BOLD, 24));
@@ -106,8 +116,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnQuatro.setBounds(111, 331, 52, 51);
+		btnQuatro.setBounds(146, 331, 52, 51);
 		contentPane.add(btnQuatro);
+		
 		// Botão 5
 		JButton btnCinco = new JButton("5");
 		btnCinco.setFont(new Font("Arial", Font.BOLD, 24));
@@ -117,8 +128,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnCinco.setBounds(173, 331, 52, 51);
+		btnCinco.setBounds(208, 331, 52, 51);
 		contentPane.add(btnCinco);
+		
 		// Botão 6
 		JButton btnSeis = new JButton("6");
 		btnSeis.setFont(new Font("Arial", Font.BOLD, 24));
@@ -128,8 +140,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnSeis.setBounds(233, 331, 52, 51);
+		btnSeis.setBounds(270, 331, 52, 51);
 		contentPane.add(btnSeis);
+		
 		// Botão sete
 		JButton btnSete = new JButton("7");
 		btnSete.setFont(new Font("Arial", Font.BOLD, 24));
@@ -139,8 +152,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnSete.setBounds(111, 272, 52, 51);
+		btnSete.setBounds(146, 272, 52, 51);
 		contentPane.add(btnSete);
+		
 		// Botão 8
 		JButton btnOito = new JButton("8");
 		btnOito.setFont(new Font("Arial", Font.BOLD, 24));
@@ -150,8 +164,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnOito.setBounds(173, 272, 52, 51);
+		btnOito.setBounds(208, 272, 52, 51);
 		contentPane.add(btnOito);
+		
 		// Botão 9
 		final JButton btnNove = new JButton("9");
 		btnNove.setFont(new Font("Arial", Font.BOLD, 24));
@@ -161,8 +176,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnNove.setBounds(233, 272, 52, 51);
+		btnNove.setBounds(270, 272, 52, 51);
 		contentPane.add(btnNove);
+		
 		// Botão Ponto
 		JButton btnPonto = new JButton(".");
 		btnPonto.setFont(new Font("Arial", Font.BOLD, 24));
@@ -172,11 +188,39 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnPonto.setBounds(173, 455, 52, 51);
+		btnPonto.setBounds(208, 455, 52, 51);
 		contentPane.add(btnPonto);
+		
+		//Botão Abre Barra
+		JButton btnAbreBarra = new JButton("(");
+		btnAbreBarra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				str.append("(");
+				atualizaTela();
+			}
+		});
+		btnAbreBarra.setFont(new Font("Arial", Font.BOLD, 24));
+		btnAbreBarra.setBounds(22, 455, 52, 51);
+		contentPane.add(btnAbreBarra);
+		
+		//Botão Fecha Barra
+		JButton btnFechaBarra = new JButton(")");
+		btnFechaBarra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				str.append(")");
+				atualizaTela();
+			}
+		});
+		btnFechaBarra.setFont(new Font("Arial", Font.BOLD, 24));
+		btnFechaBarra.setBounds(84, 455, 52, 51);
+		contentPane.add(btnFechaBarra);
+		
+		
 		/*
 		 * Operadores
 		 */
+		
+		
 		// Botão Multiplicação
 		JButton btnMultiplicacao = new JButton("*");
 		btnMultiplicacao.setFont(new Font("Arial", Font.BOLD, 24));
@@ -187,8 +231,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnMultiplicacao.setBounds(295, 331, 52, 51);
+		btnMultiplicacao.setBounds(332, 331, 52, 51);
 		contentPane.add(btnMultiplicacao);
+		
 		// Botão Subtração
 		JButton btnSubtracao = new JButton("-");
 		btnSubtracao.setFont(new Font("Arial", Font.BOLD, 24));
@@ -199,8 +244,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnSubtracao.setBounds(295, 393, 52, 51);
+		btnSubtracao.setBounds(332, 393, 52, 51);
 		contentPane.add(btnSubtracao);
+		
 		// Botão Soma
 		JButton btnSoma = new JButton("+");
 		btnSoma.setFont(new Font("Arial", Font.BOLD, 24));
@@ -211,8 +257,9 @@ public class Calculadora extends JFrame {
 				atualizaTela();
 			}
 		});
-		btnSoma.setBounds(233, 455, 52, 51);
+		btnSoma.setBounds(270, 455, 52, 51);
 		contentPane.add(btnSoma);
+		
 		// Botão Divisão
 		JButton btnDivisao = new JButton("/");
 		btnDivisao.addActionListener(new ActionListener() {
@@ -220,19 +267,25 @@ public class Calculadora extends JFrame {
 				str.append("/");
 			}
 		});
+		btnDivisao.setFont(new Font("Arial", Font.BOLD, 24));
+		btnDivisao.setBounds(332, 272, 52, 51);
+		contentPane.add(btnDivisao);
+		
 		// Botão Igual
-		JButton btnIgual = new JButton("=");
+		btnIgual = new JButton("=");
 		btnIgual.setFont(new Font("Arial", Font.BOLD, 24));
 		btnIgual.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String op = str.toString();
+				String op = ""+operacao(str);
 				clear();
-				str.append(operacao(op));
+				str.append(op);
 				atualizaTela();
 			}
 		});
-		btnIgual.setBounds(295, 455, 52, 51);
+		btnIgual.setBounds(332, 455, 52, 51);
 		contentPane.add(btnIgual);
+		
+		
 
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 644, 21);
@@ -261,9 +314,11 @@ public class Calculadora extends JFrame {
 		txtAreaCalculo.setEditable(false);
 		txtAreaCalculo.setFont(new Font("Arial", Font.BOLD, 30));
 		txtAreaCalculo.setHorizontalAlignment(SwingConstants.RIGHT);
-		txtAreaCalculo.setBounds(10, 32, 337, 51);
+		txtAreaCalculo.setBounds(10, 32, 374, 51);
 		contentPane.add(txtAreaCalculo);
 		txtAreaCalculo.setColumns(10);
+		
+		
 		// Botão Limpar
 		JButton btnLimpar = new JButton("Limpar");
 		btnLimpar.setFont(new Font("Arial", Font.BOLD, 24));
@@ -272,8 +327,10 @@ public class Calculadora extends JFrame {
 				clear();
 			}
 		});
-		btnLimpar.setBounds(233, 94, 114, 42);
+		btnLimpar.setBounds(270, 94, 114, 42);
 		contentPane.add(btnLimpar);
+		
+		
 		// Botão apagar um
 		JButton btnApagarUm = new JButton("");
 		btnApagarUm.addActionListener(new ActionListener() {
@@ -284,12 +341,35 @@ public class Calculadora extends JFrame {
 		});
 		btnApagarUm.setIcon(new ImageIcon(Calculadora.class
 				.getResource("/br/edu/edi/images/flecha_esquerda.png")));
-		btnApagarUm.setBounds(295, 219, 52, 42);
+		btnApagarUm.setBounds(332, 219, 52, 42);
 		contentPane.add(btnApagarUm);
-
-		btnDivisao.setFont(new Font("Arial", Font.BOLD, 24));
-		btnDivisao.setBounds(295, 272, 52, 51);
-		contentPane.add(btnDivisao);
+		
+		JButton btnRaizQuadrada = new JButton("\u221A");
+		btnRaizQuadrada.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				str.append("sqrt(");
+				atualizaTela();
+				
+				
+				
+			
+			}
+		});
+		btnRaizQuadrada.setFont(new Font("Arial", Font.BOLD, 24));
+		btnRaizQuadrada.setBounds(84, 393, 52, 51);
+		contentPane.add(btnRaizQuadrada);
+		
+		JButton btnX = new JButton("x\u00B2");
+		btnX.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				str.append("^2");
+				atualizaTela();
+			}
+		});
+		btnX.setFont(new Font("Arial", Font.BOLD, 20));
+		btnX.setBounds(22, 393, 52, 51);
+		contentPane.add(btnX);
+		
 
 	}
 
@@ -337,20 +417,51 @@ public class Calculadora extends JFrame {
 	/*
 	 * Realiza operações de cálculo
 	 */
-	public Object operacao(String calculo) {
-
+	public Object operacao(StringBuffer str2) {
 		// create a script engine manager
 		ScriptEngineManager factory = new ScriptEngineManager();
 		// create a JavaScript engine
 		ScriptEngine engine = factory.getEngineByName("JavaScript");
 		// evaluate JavaScript code from String
+		
+		String equacao = teste(str2);	
+		
 		Object obj = null;
 		try {
-			obj = engine.eval(calculo);
+			obj = engine.eval(equacao);
 		} catch (ScriptException e) {
 			return 0;
 		}
 		return obj;
+	}
+	
+	private String teste(StringBuffer str2){
+		int contAbre = 0;
+		int contFecha = 0;
+		
+		for (int i = 0; i < str2.length(); i++) {
+			if (str2.charAt(i) == '(') {
+				contAbre++;	
+			}
+			if(str2.charAt(i)== ')'){
+				contFecha++;
+			}
+			
+		}
+		
+		if(contFecha<contAbre){
+			int dif = contAbre-contFecha;
+			for (int i = 0; i < dif; i++) {
+				str2.append(")");
+			}
+		}
+		
+		String str = str2.toString();
+		str = str.replaceAll("sqrt", "Math.sqrt");
+		
+		return str;
+		
+		
 	}
 
 	/*
